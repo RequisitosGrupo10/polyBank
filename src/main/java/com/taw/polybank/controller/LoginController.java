efcf80bbd38791686a3994dd5c60835c75ffd11f
Illya Rozumovskyy <75805551+illyaro@users.noreply.github.com>
2023-04-03 22:00:05 +0200

    Unused imports removed

--- src/main/java/com/taw/polybank/controller/LoginController.java
+++ src/main/java/com/taw/polybank/controller/LoginController.java
@@ -1,24 +1,8 @@
 package com.taw.polybank.controller;
 
-import com.taw.polybank.dao.*;
-import com.taw.polybank.entity.*;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.nio.charset.Charset;
-import java.security.SecureRandom;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.List;
-import java.util.Random;
 
 @Controller
 public class LoginController {
