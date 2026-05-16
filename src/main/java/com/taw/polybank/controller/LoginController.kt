07688033e5f8b92ebb4e3ae43734414913bd8d1d
Illya Rozumovskyy <illya.rozumovskyy@glovoapp.com>
2026-05-16 21:47:01 +0200

    Migrated Login to kotlin

---
+++ src/main/java/com/taw/polybank/controller/LoginController.kt
@@ -0,0 +1,33 @@
+package com.taw.polybank.controller
+
+import com.taw.polybank.dao.AuthorizedAccountRepository
+import com.taw.polybank.dao.BankAccountRepository
+import com.taw.polybank.dao.ClientRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+/**
+ * @author José Manuel Sánchez Rico
+ */
+@Controller
+class LoginController {
+  @Autowired
+  private val authorizedAccountRepository: AuthorizedAccountRepository? = null
+
+  @Autowired
+  private val clientRepository: ClientRepository? = null
+
+  @Autowired
+  private val bankAccountRepository: BankAccountRepository? = null
+
+  @GetMapping("/")
+  fun doShowIndex(): String {
+    return "index"
+  }
+
+  @GetMapping("/login")
+  fun doLogin(): String {
+    return ("login")
+  }
+}
